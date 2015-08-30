304206f6f9f4c31303436403106befbe3333554b
hausen6 <hausen62480@gmail.com>
2015-08-30 17:12:43 +0900

    vimのセットアップ用設定を追加

--- vim_setup.sh
+++ vim_setup.sh
@@ -26,6 +26,15 @@ cd vim
 make reconfig
 sudo make install
 
+# 自分用のvim setting
+# vim 用directoryの作成
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/vimbackup
+
+# neobundle install
+cd ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim
+
 # もしも./configure でlibluajitがないと言われたら以下をコメントイン
 # git clone https://luajit.org/git/luajit-2.0.git
 # cd luajit-2.0
